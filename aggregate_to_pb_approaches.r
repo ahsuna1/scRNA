304401550aaafbb29337c135e76f2903eabed201
anusha <45042988+ahsuna1@users.noreply.github.com>
2022-07-06 16:12:11 +0530

    Update aggregate_to_pb_approaches.r

--- aggregate_to_pb_approaches.r
+++ aggregate_to_pb_approaches.r
@@ -118,26 +118,26 @@ to_pseudobulk = function(input,
       
       # process data into gene X replicate X cell_type matrice
       
-      mat_mm = parallel::lcmapply(
-        unique(replicate:label, data = meta0),
-        if (fun == "sum"){
-          function(x){Matrix::rowSums(SingleCellExperiment::counts(sce[, replicate:label, data == x]))},
-            mc.cores = future::availableCores())
-          } else if (fun == "mean"){
-          function(x){Matrix::rowMeans(SingleCellExperiment::counts(sce[, replicate:label, data == x]))},
-            mc.cores = future::availableCores())
-          } else{
-          function(x){Matrix::rowMedians(SingleCellExperiment::counts(sce[, replicate:label, data == x]))},
-            mc.cores = future::availableCores())
-          }
-          pb_matrix <- Reduce(cbind, pb_matrix)
-          colnames(pb_matrix) <- unique(replicate:label, data = meta0)
+      #mat_mm = parallel::lcmapply(
+       # unique(replicate:label, data = meta0),
+        #if (fun == "sum"){
+         # function(x){Matrix::rowSums(SingleCellExperiment::counts(sce[, replicate:label, data == x]))},
+          #  mc.cores = future::availableCores())
+          #} else if (fun == "mean"){
+          #function(x){Matrix::rowMeans(SingleCellExperiment::counts(sce[, replicate:label, data == x]))},
+           # mc.cores = future::availableCores())
+          #} else{
+          #function(x){Matrix::rowMedians(SingleCellExperiment::counts(sce[, replicate:label, data == x]))},
+           # mc.cores = future::availableCores())
+          #}
+          #pb_matrix <- Reduce(cbind, pb_matrix)
+          #colnames(pb_matrix) <- unique(replicate:label, data = meta0)
           
         
       
       
-      #mm = model.matrix(~ 0 + replicate:label, data = meta0)
-      #mat_mm = expr0 %*% mm
+      mm = model.matrix(~ 0 + replicate:label, data = meta0)
+      mat_mm = expr0 %*% mm
       keep_genes = if (fun == "sum"){
         Matrix::rowSums(mat_mm > 0) > min_features
         } else if (fun == "mean") {
